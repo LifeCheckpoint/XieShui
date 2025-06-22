36175585aafc89638e372164d7e11b9d4476f23b
dht <dhy4568527913@163.com>
2025-06-22 20:23:10 +0800

    又实现了一些方法

--- backend/knowledge_graph.py
+++ backend/knowledge_graph.py
@@ -5,7 +5,7 @@ from typing import Optional, Dict, Any,List
 class Knowledge_Node(BaseModel):
     id: str
     name: str
-    description: Optional[str] = None
+    description: Optional[str] = None #optional的意思是可以没有，有的话变量类型就是str
     content: Optional[Any] = None
     in_edge:List[str] = []
     out_edge:List[str] = []
@@ -52,6 +52,81 @@ class Knowledge_Graph(BaseModel):
             self.edges[edge.id] = edge
             edge.start_node.out_edge.append(edge.id)
             edge.end_node.in_edge.append(edge.id)
+    
+    def get_node(self,node_id:str):
+        return self.nodes.get(node_id)
+    
+    def get_edge(self,edge_id:str):
+        return self.edges.get(edge_id)
+    
+    def get_all_node(self):
+        node_list = list(self.nodes.values())
+        return node_list
+    
+    def get_all_edge(self):
+        edge_list = list(self.edges.values())
+        return edge_list
+    
+    def get_out_node(self,node_id:str):
+        node = self.nodes[node_id]
+        if self.verify_node(node) == -1:
+            print('Valueerror')
+            exit(0)
+        else:
+            out_edge_list = [self.nodes[id] for id in node.out_edge]
+            return out_edge_list
+    
+    def get_in_node(self,node_id:str):
+        node = self.nodes[node_id]
+        if self.verify_node(self,node) == -1:
+            print("ValueError")
+            exit(0)
+        else:
+            in_edge_list = [self.nodes[id] for id in node.in_edge]
+            return in_edge_list
+    
+    def get_neighbours(self,node_id:str):
+        node = self.nodes[node_id]
+        if self.verify_node(self,node) == -1:
+            print("ValueError")
+            exit(0)
+        else:
+            node_list = []
+            for edge_id in node.in_edge:
+                edge = self.edges[edge_id]
+                node_list.append(edge.start_node)
+                node_list.append(edge.end_node)
+            for edge_id in node.out_edge:
+                edge = self.edges[edge.id]
+                node_list.append(edge.start_node)
+                node_list.append(edge.end_node)
+            
+            node_list = list(set(node_list))
+            node_list.remove(node)
+            return node_list
+        
+    def remove_node(self,node_id:str):
+        node = self.nodes[node_id]
+        if self.verify_node(self,node) == -1:
+            print("ValueError")
+            exit(0)
+        else:
+            for edge_id in node.in_edge:
+                edge = self.edges[edge_id]
+                edge.start_node.out_edge.remove(edge_id)
+                del self.edges[edge_id]
+            
+            for egde_id in node.out_edge:
+                edge = self.edges[edge_id]
+                edge.end_node.in_edge.remove(edge_id)
+                del self.edges[edge_id]
+            
+            del self.nodes[node_id]
+                
+                
+        
+        
+    
             
         
             
